b104e343fdf84a06576c434b52eb481f349bb0b3
razibul1995 <razibulhasan1995@gmail.com>
2022-04-03 01:53:22 +0600

    Add project files.

---
+++ OOP Example1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Example1
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+
+        public string GetFullname()
+        {
+            string fullname = firstName + " " + middleName + " " + lastName;
+
+            return fullname;
+
+        }
+    }
+
+    
+}
---
+++ OOP Example1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            p1.firstName = "Md.";
+            p1.middleName = "Razibul";
+            p1.lastName = "Hasan";
+
+            string fullname = p1.GetFullname();
+            Console.WriteLine(fullname);
+            Console.ReadKey();
+        }
+    }
+}
